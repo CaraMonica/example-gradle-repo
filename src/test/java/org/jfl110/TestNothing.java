b968811502f9d3f77caed5b4d1e1f80061619b46
JFL110 <typus24@hotmail.com>
2020-02-18 21:24:29 +0100

    Initial commit

---
+++ src/test/java/org/jfl110/TestNothing.java
@@ -0,0 +1,19 @@
+package org.jfl110;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test to test the tests.
+ * 
+ * @author jim
+ *
+ */
+public class TestNothing {
+
+	@Test
+	public void testNothing() {
+		assertTrue(true);
+	}
+}
